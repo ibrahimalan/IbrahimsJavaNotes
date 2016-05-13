1e4622c0043d82e9a23ce0523622aa17a59431d4
ibralan <yezders@gmail.com>
2016-05-13 09:50:36 +0300

    Variables For Start Added

---
+++ Startingjava/src/com/ibralan/works/beginning/StartForVariables.java
@@ -0,0 +1,31 @@
+package com.ibralan.works.beginning;
+
+/**
+ * @author Ibrahim Alan
+ *         http://ibralan.com
+ *         13.05.2016 - 09:36
+ */
+public class StartForVariables
+{
+    public static void main(String[] args)
+    {
+        {   //Variables for String
+
+            String isim = "Ibrahim";
+
+            System.out.println("Hey" + isim);
+            System.out.println("How are you" + isim);
+            System.out.println("What are you doing" + isim);
+
+        }
+
+        {   //Variables for int
+
+            int a = 1;
+            int b = 2;
+            int c = (b + a) * 5;
+
+            System.out.println((c + a) / 4);
+        }
+    }
+}

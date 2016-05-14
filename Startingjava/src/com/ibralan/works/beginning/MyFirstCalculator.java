52ba7ecd6caa8462f389c8718aadfaac7b373437
ibralan <yezders@gmail.com>
2016-05-14 14:11:11 +0300

    Calculator Changed

--- Startingjava/src/com/ibralan/works/beginning/MyFirstCalculator.java
+++ Startingjava/src/com/ibralan/works/beginning/MyFirstCalculator.java
@@ -9,48 +9,36 @@ import java.util.Scanner;
  */
 public class MyFirstCalculator
 {
-
     public static void main(String[] args)
-
     {
         while (true)
-
         {
-        boolean works = true;
+            int x, y;
+            Scanner scan = new Scanner(System.in);
 
+            System.out.print("First Number: ");
+            x = scan.nextInt();
+
+            System.out.print("Second Number: ");
+            y = scan.nextInt();
+
+            System.out.println("Choose operation: +, -, /, x ");
+            Scanner scan2 = new Scanner(System.in);
+            String calc = scan2.nextLine();
+
+            if (calc.equalsIgnoreCase("+"))
             {
-                int x, y;
-                Scanner scan = new Scanner(System.in);
-
-                System.out.print("First Number: ");
-                x = scan.nextInt();
-
-                System.out.print("Second Number: ");
-                y = scan.nextInt();
-
-
-                System.out.println("Results: ");
-                String calc = scan.nextLine();
-
-                if (calc == ("Plus") || works)
-                {
-                    System.out.println("Answer For Plus: " + (x + y));
-                }
-                if (calc == ("Minus") || works)
-                {
-                    System.out.println("Answer For Minus: " + (x - y));
-                }
-                if (calc == ("Multiply") || works)
-                {
-                    System.out.println("Answer For Multiply: " + (x * y));
-                }
-                if (calc == ("Divided by") || works)
-                {
-                    System.out.println("Answer For Divided by: " + (x / y));
-                }
-
+                System.out.println("Answer For Plus: " + (x + y));
+            } else if (calc.equalsIgnoreCase("-"))
+            {
+                System.out.println("Answer For Minus: " + (x - y));
+            } else if (calc.equalsIgnoreCase("*"))
+            {
+                System.out.println("Answer For Multiply: " + (x * y));
+            } else if (calc.equalsIgnoreCase("/"))
+            {
+                System.out.println("Answer For Divided by: " + (x / y));
             }
         }
     }
-
 }
